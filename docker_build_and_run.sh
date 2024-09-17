5a2680e961103cef16aaea0e757c9d1a59b3c8fc
Muhammad Shoaib Sikander <shoaib.pu@gmail.com>
2024-09-17 16:37:52 +0200

    added example-6

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -180,3 +180,42 @@ docker rmi image_2a_simple_addition
 echo "-----"
 
 echo "======================================="
+
+echo "******* Example-6 *******"
+
+# build docker image
+docker build -f docker_file_2a_simple_addition -t image_2a_simple_addition .
+docker build -f docker_file_2b_simple_addition -t image_2b_simple_addition .
+echo "-----"
+
+# delete code_operation.py python file if it exists
+if [ -e code_operation.py ]
+then
+    echo "code_operation.py file exists ..."
+    rm code_operation.py
+    echo "code_operation.py file deleted ..."
+else
+    echo "code_operation.py file does not exist ..."
+fi
+echo "-----"
+
+# run docker image as container, extract code_operation.py python file, stop & remove the container, delete the docker image
+docker compose -f docker_compose_file_2b.yml up -d
+docker cp container_2b_simple_addition:/my_dir/code_operation.py code_operation.py
+docker compose -f docker_compose_file_2b.yml down
+docker rmi image_2b_simple_addition
+echo "-----"
+
+# run docker image as container
+docker compose -f docker_compose_file_2a.yml up
+echo "-----"
+
+# remove docker container
+docker compose -f docker_compose_file_2a.yml down
+echo "-----"
+
+# remove docker image
+docker rmi image_2a_simple_addition
+echo "-----"
+
+echo "======================================="
